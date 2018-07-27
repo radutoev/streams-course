f4c479b041ba23f72c630541eeeb02e7eb910530
Radu Toev <radu.toev@siemens.com>
2018-07-27 17:05:16 +0300

    With toList, toMap

--- src/com/siemens/ifa/Main.java
+++ src/com/siemens/ifa/Main.java
@@ -1,12 +1,10 @@
 package com.siemens.ifa;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
@@ -16,47 +14,63 @@ public class Main {
     public static void main(String[] args) {
         final List<Integer> numbers = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        //double the even values and sum them
-        int result = 0;
-        for (Integer number : numbers) {
-            if (number % 2 == 0) { //filtrare
-                final int x = number * 2; //mapare x -> x`
-                result += x;
-            }
-        }
-        System.out.println(result);
-
-        //internal iterator
-        System.out.println(numbers
-                .stream() //stream init
-                //aplicare de functii -> functional composition
-                .filter(n -> n % 2 == 0)
-                .map(n -> n * 2)
-                //terminal operation, stream termination
-                .reduce(0, (acc, x) -> acc += x));
-
         List<Person> people = generatePeople();
 
-//        final Predicate<Integer> gt25 = new Predicate<Integer>() {
-//            @Override
-//            public boolean test(Integer integer) {
-//                return integer > 25;
-//            }
-//        };
-        final Predicate<Integer> gt25 = age -> age > 25;
 
-        TimeIT.code(() ->
-            people.parallelStream()
-                    .map(p -> compute(p))
-                    .map(Person::age) //Person -> age (int)
-                    .filter(gt25) //age(int)
-                    .reduce(0, (acc, x) -> acc += x)
-        );
+        //collect: toList, toSet, toMap.
 
-        String names = people.stream()
-                .map(p -> p.getName())
-                .reduce("", (carry, n) -> carry += n + ",");
-        System.out.println(names.substring(0, names.length() - 1));
+        //ex: double of even numbers
+//        List<Integer> doubleOfEvents = new ArrayList<>();
+        List<Integer> doubleOfEvents  = numbers.stream()
+                .filter(e -> e % 2 == 0)
+                .map(e -> e * 2)
+//                .forEach(doubleOfEvents::add); //shared mutability
+                .collect(Collectors.toList());
+        System.out.println(doubleOfEvents);
+
+        //nr of operations?? 18
+        List<String> names = new ArrayList<>();
+        for (Person person : people) {
+            if(person.age() > 25) {
+                names.add(person.getName());
+            }
+        }
+
+        //
+        people.stream() //list size 6
+            .filter(p -> p.age() > 25) //list size 4
+            .map(Person::getName)
+            .collect(Collectors.toList());
+
+        /**
+         *  MALE ->
+         *
+         */
+        Map<Sex, List<Person>> map = people.stream()
+                .collect(Collectors.groupingBy(p -> p.getSex()));
+        System.out.println(map);
+
+//
+////        final Predicate<Integer> gt25 = new Predicate<Integer>() {
+////            @Override
+////            public boolean test(Integer integer) {
+////                return integer > 25;
+////            }
+////        };
+//        final Predicate<Integer> gt25 = age -> age > 25;
+//
+//        TimeIT.code(() ->
+//            people.parallelStream()
+//                    .map(p -> compute(p))
+//                    .map(Person::age) //Person -> age (int)
+//                    .filter(gt25) //age(int)
+//                    .reduce(0, (acc, x) -> acc += x)
+//        );
+//
+//        String names = people.stream()
+//                .map(p -> p.getName())
+//                .reduce("", (carry, n) -> carry += n + ",");
+//        System.out.println(names.substring(0, names.length() - 1));
     }
 
     static Person compute(Person p) {
